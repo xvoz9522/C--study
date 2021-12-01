b252b6761851a723ba9bee58fcaf4f83e0eaaf4a
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-02 00:15:55 +0800

    Add files via upload

--- C++.2019.BlackHors.test/C++.2019.BlackHors.test/Less01.cpp
+++ C++.2019.BlackHors.test/C++.2019.BlackHors.test/Less01.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 
 #define Day 7
-int main() 
-{
-	int A = 10;
-
-	cout << "㏄Τ碭ぱ: " << Day << "ぱ" << endl;
-
-	const int MONTH = 12;
-
-	cout << "Τぶる: " << MONTH << "る " << endl;
-	//int num1 = 10;
-	//int num2 = 20;
-	//int sum = num1 + num2;
-	//cout << sum << endl;
-	//祏俱计(-32768~32767) 
-	short a = 10;
-	//俱计
-	int b = 10;
-	//俱计
-	long c = 10;
-	//俱计
-	long long d = 10;
-
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
-	cout << "c = " << c << endl;
-	cout << "d = " << d << endl;
-
-	system("pause");
-
-	return 0;
-}
+//int main() 
+//{
+//	int A = 10;
+//
+//	cout << "㏄Τ碭ぱ: " << Day << "ぱ" << endl;
+//
+//	const int MONTH = 12;
+//
+//	cout << "Τぶる: " << MONTH << "る " << endl;
+//	//int num1 = 10;
+//	//int num2 = 20;
+//	//int sum = num1 + num2;
+//	//cout << sum << endl;
+//	//祏俱计(-32768~32767)					 
+//	short a = 10;
+//	//俱计
+//	int b = 10;
+//	//俱计
+//	long c = 10;
+//	//俱计
+//	long long d = 10;
+//
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl;
+//	cout << "d = " << d << endl;
+//
+//	system("pause");
+//
+//	return 0;
+//}
---
+++ C++.2019.BlackHors.test/C++.2019.BlackHors.test/tset.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+
+	char v = 'A' + 32;
+
+	int a = 50;
+	int e = 70;
+	int sum01 = a + e;
+
+	cout << "Hello World " << sum01 << v << endl;
+	int b = 70;
+
+	double c = (a + b) / 2.;
+
+	cout << c << endl;
+	double ar = 7.6;
+	cout << sizeof(ar) << endl;
+
+	cout << "HelloWorld\n";
+
+
+
+	system("pause");
+	return 0;
+}
