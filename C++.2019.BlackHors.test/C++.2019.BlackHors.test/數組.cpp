1f71c57142fd4008b6ff1dd33ffe4b60699e5b70
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-12 23:45:14 +0800

    Add files via upload

---
+++ C++.2019.BlackHors.test/C++.2019.BlackHors.test/數組.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//
+//	int arr[5];
+//	//數組開頭都是從零開始
+//	arr[0] = 10;
+//	arr[1] = 20;
+//	arr[2] = 30;
+//	arr[3] = 40;
+//	arr[4] = 50;
+//	//cout << arr[0] << endl;
+//	//cout << arr[1] << endl;
+//	//cout << arr[2] << endl;
+//	//cout << arr[3] << endl;
+//	//cout << arr[4] << endl;
+//	// 
+//	//如果在初始化元素時,沒有填寫的地方,就會用0代替
+//	int arr2[5] = { 10,20,30,40,50 };
+//	//cout << arr2[0] << endl;
+//	//cout << arr2[1] << endl;
+//	//cout << arr2[2] << endl;
+//	//cout << arr2[3] << endl;
+//	//cout << arr2[4] << endl;
+//	//利用循環,輸出數組的元素
+//	//for (int i = 0; i < 5; i++)
+//	//{
+//	//	cout << arr2[i] << endl;
+//	//}
+//	//寫數組時,一定要有內容
+//	int arr3[] = { 10,20,30 };
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << arr3[i] << endl;
+//	}
+//
+//	system("pause");
+//	return 0;
+//
+//}
