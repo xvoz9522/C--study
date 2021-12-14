df95a3430d0ecdfde3961af437508a5bab72dbb7
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-15 00:35:15 +0800

    Add files via upload

--- C++.2019.BlackHors.test/C++.2019.BlackHors.test/數組名.cpp
+++ C++.2019.BlackHors.test/C++.2019.BlackHors.test/數組名.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
-using namespace std;
-int main(){
-
-
-
-    system("pause");
-    return 0;
-
-}
+//#include <iostream>
+//using namespace std;
+//int main(){
+//
+//    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//    cout << "內存空間為: " << sizeof(arr) << endl;
+//    //空間=整數X內存位[]
+//    cout << "每個數組元素空間: " << sizeof(arr[0]) << endl;
+//    cout << "每個數組元素個數為: " << sizeof(arr) / sizeof(arr[0]) << endl;
+//    cout << "數組首地址為: " << arr << endl;
+//    cout << "數組首地址為: " << (int)arr << endl;
+//    cout << "數組第一個元素地址為: " << (int) & arr[0] << endl;
+//    cout << "數組第2個元素地址為: " << (int)&arr[1] << endl;
+//    //數組名是一個常量,不行進行賦值動作
+//    system("pause");
+//    return 0;
+//
+//}
