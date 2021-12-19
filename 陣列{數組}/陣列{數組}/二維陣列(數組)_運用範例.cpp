9baa3d0e7b632bb1fabcc4cb0d9a4e23f5ca5670
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-19 16:05:41 +0800

    Add files via upload

--- 陣列{數組}/陣列{數組}/二維陣列(數組)_運用範例.cpp
+++ 陣列{數組}/陣列{數組}/二維陣列(數組)_運用範例.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
-using namespace std;
-int main() {
-
-
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	
+//	int scores[3][3] = {
+//		{100,100,100} ,
+//		{90,50,100},
+//		{60,70,80}
+//	};
+//
+//	string names[3] = { "小明","育德","情真" };
+//
+//	for (int i = 0; i < 3; i++)
+//	{
+//		int sum = 0;//分數總和的變量
+//		for (int j = 0; j < 3; j++)
+//		{
+//			sum += scores[i][j];
+//			//cout << scores[i][j] << " ";
+//		}
+//		cout << names[i] << "總分 " << sum << endl;
+//	}
+//	
+//
+//	system("pause");
+//	return 0;
+//}
