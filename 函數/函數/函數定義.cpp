7fecb75cc2817c1aa74b0d23f16b75ea2ecb5cd5
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-19 23:01:01 +0800

    Add files via upload

---
+++ 函數/函數/函數定義.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+////ㄧ计﹚竡
+////粂猭:
+////摸,ㄧ计(把计),{ㄧ计砰粂 return笷Α}
+////猭ㄧ计
+//int add(int num1, int num2) {
+//
+//	int sum = num1 + num2;
+//	return sum;
+//
+//}
+//
+//int main() {
+//	/*int add(int num, int num2)*/
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 函數/函數/函數調用.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+////函數在定義時,num1 and num2沒有實際數值
+////他只是一個形式上的參數,簡稱形參
+//int add(int num1, int num2) {
+//	
+//		int sum = num1 + num2;
+//		return sum;
+//	}
+//int main() {
+//
+//	int a = 10;
+//	int b = 20;
+//	
+//	int c = add(a, b);
+//	cout << "c = " << c << endl;
+//	//a和b稱為實際參數,簡稱實參
+//	//當調用函數時,實參的值會傳遞給形參
+//	a = 100;
+//	b = 600;
+//	c = add(a, b);
+//	cout << "c = " << c << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
