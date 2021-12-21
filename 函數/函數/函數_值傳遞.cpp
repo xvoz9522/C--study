880253063bd41e5f44de43725d86de6175dd316a
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-22 00:21:52 +0800

    Add files via upload

---
+++ 函數/函數/函數_值傳遞.cpp
@@ -0,0 +1,34 @@
+//#include <iostream>
+//using namespace std;
+//
+////﹚竡ㄧ计,ㄢ计ユ传ㄧ计
+////狦ㄧ计ぃ惠璶ノ.羘碞ノvoid
+//void swap(int num1, int num2)
+//{
+//	cout << "ユ传玡: " << endl;
+//	cout << "num1: " << num1 << endl;
+//	cout << "num2: " << num2 << endl;
+//
+//	int tmap = num1;
+//	num1 = num2;
+//	num2 = tmap;
+//
+//	cout << "ユ传: " << endl;
+//	cout << "num1: " << num1 << endl;
+//	cout << "num2: " << num2 << endl;
+//
+//}
+//int main() {
+//
+//	int a = 10;
+//	int b = 20;
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;
+//	//讽иㄏノ肚患,ㄧ计把祇ネэ跑,ぃ穦紇臫龟把
+//	swap(a, b);
+//	cout << "a= " << a << endl;
+//	cout << "b= " << b << endl;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 函數/函數/函數常見樣式.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//無參無反
+void test01()
+{
+	cout << "this is test01 " << endl;
+}
+
+int main()
+{
+	//無參無反
+	test01();
+
+
+	system("pause");
+	return 0;
+}
