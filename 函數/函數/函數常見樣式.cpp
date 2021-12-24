17cf13678f18b840c9c7b56cce42774453afb57b
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-25 00:45:56 +0800

    Add files via upload

--- 函數/函數/函數常見樣式.cpp
+++ 函數/函數/函數常見樣式.cpp
@@ -1,17 +1,42 @@
-#include <iostream>
-using namespace std;
-//礚把礚は
-void test01()
-{
-	cout << "this is test01 " << endl;
-}
-
-int main()
-{
-	//礚把礚は
-	test01();
-
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+////礚把礚は
+//void test01()
+//{
+//	cout << "this is test01 " << endl;
+//}
+////Τ把礚は
+//void test02(int a)
+//{
+//	cout << "This is test02 a = " << a << endl;
+//}
+////礚把Τは
+//int test03()
+//{
+//	cout << "This is test03 " << endl;
+//	return 754;
+//}
+////Τ把Τは
+//int test04(int b)
+//{
+//	cout << "This is test04 b = " << b << endl;
+//	return b;
+//}
+//
+//int main()
+//{
+//	//礚把礚はㄧ计秸ノ
+//	test01();
+//	//Τ把礚はㄧ计秸ノ
+//	test02(100);
+//	//礚把Τはㄧ计秸ノ
+//	int num1 = test03();
+//	cout <<"num1= " << num1 << endl;
+//	//Τ把Τはㄧ计秸ノ
+//	int num2 = test04(69);
+//	cout << "num2= " << num2 << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
