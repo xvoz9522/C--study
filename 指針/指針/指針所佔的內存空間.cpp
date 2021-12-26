7eec296ed5b9895c38879785fed20bec49100824
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2021-12-26 16:34:48 +0800

    Add files via upload

---
+++ 指針/指針/指針所佔的內存空間.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	//指針所站內存空間
+//	//指針在32為操作系統下,指針式站4個字節空間大小,不論任何數據類型
+//	//指針在64為操作系統下,指針式站8個字節空間大小,不論任何數據類型
+//	cout << "sizeof (int*) = " << sizeof(int*) << endl;
+//	cout << "sizeof (float*) = " << sizeof(float*) << endl;
+//	cout << "sizeof (double*) = " << sizeof(double*) << endl;
+//	cout << "sizeof (char*) = " << sizeof(char*) << endl;
+//	system("pause");
+//	return 0;
+//}
---
+++ 指針/指針/空指針和野指針.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//
+//	//1.空指針用於給指針變量進行初始化
+//	//int* p = NULL; 
+//	//2.空指針是不可以進行訪問的
+//	//0~255之間之間的內存編號是系統占用的,因此不可以訪問
+//	//*p = 100;
+//	//野指針
+//	//int* p = (int*)0x1100;
+//	//cout << *p << endl;
+//	system("pause");
+//	return 0;
+//}
