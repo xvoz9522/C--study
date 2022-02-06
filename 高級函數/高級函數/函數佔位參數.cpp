c212ce14b40fb38dc68c86e4ed905eee89222ca1
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2022-02-06 19:57:46 +0800

    Add files via upload

---
+++ 高級函數/高級函數/函數佔位參數.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+////ㄧ计
+////摸 ㄧ计(计沮摸){}
+//
+////ヘ玡顶琿把计 и临ノぃ 揭祘い穦ノ
+////把计临Τ纐粄把计
+//void func(int a, int)
+//{
+//	cout << "This is func " << a << endl;
+//}
+//
+//
+//int main()
+//{
+//
+//	func(10,10);
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 高級函數/高級函數/函數默認參數.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//using namespace std;
+//
+////ㄧ计纐粄把计
+//
+////狦и肚计沮,碞ノ计沮,狦⊿Τ,碞ノ纐粄
+////粂猭: 摸 ㄧ计嘿 (把 = 纐粄) {}
+//int func(int a ,int b = 20,int c = 80)
+//{
+//	return a + b + c;
+//}
+//
+//
+//
+////猔種ㄆ兜
+////1.狦琘竚Τ纐粄把计,ê或硂竚┕,眖オゲ斗常琌纐粄
+////int func(int a, int b, int c, int d)
+////{
+////	return a + b + c;
+////}
+//
+////2.狦ㄧ计羘Τ纐粄把计,ㄧ计龟瞷碞ぃΤ纐粄把计
+//int func2(int a = 10, int b = 10);
+//
+//int func2(int a , int b )
+//{
+//	return a + b;
+//}
+//
+//
+//
+//int main()
+//{
+//
+//	//cout << func(10,30) << endl;
+//	cout << func2() << endl;
+//
+//	system("pause");
+//	return 0;
+//}
