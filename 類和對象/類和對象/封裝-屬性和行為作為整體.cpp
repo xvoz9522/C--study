8ffcdce26d16a21b7d691c6d8dc8fb7ec400ceab
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2022-02-13 21:41:12 +0800

    Add files via upload

---
+++ 類和對象/類和對象/封裝-屬性和行為作為整體.cpp
@@ -0,0 +1,49 @@
+//#include <iostream>
+//using namespace std;
+//
+////圓周率
+//const double PI = 3.14;
+//
+////設計一個圓類,來求一個圓的周長
+////求圓的周長:2 * 3.14 * 半徑
+//
+////class 代表設計一個類, 類後面緊跟著的就是類名稱
+//
+//class Circle
+//{
+////訪問權限
+////公共權限
+//public:
+//	//屬性
+//	//半徑
+//	int m_r;
+//	
+//	//行為
+//	//獲取圓的周長
+//	double calculateZC()
+//	{
+//		return 2 * PI * m_r;
+//	}
+//
+//
+//
+//private:
+//
+//};
+//
+//
+//
+//int main()
+//{
+//
+//	//通過圓類 創建具體的圓(對象)
+//	//實例化(通過一個類 創建一個對象的過程)
+//	Circle c1;
+//	//給圓的對象 的屬性進行賦值
+//	c1.m_r = 10;
+//
+//	cout << "圓周長: " << c1.calculateZC() << endl;
+//
+//	system("pause");
+//	return 0;
+//}
