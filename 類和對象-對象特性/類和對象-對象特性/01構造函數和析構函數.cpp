28453cde87236b406930a59f9dcc4a0ea970a003
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2022-02-22 00:50:18 +0800

    Add files via upload

---
+++ 類和對象-對象特性/類和對象-對象特性/01構造函數和析構函數.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+//癸禜﹍て㎝睲瞶
+//1.篶硑ㄧ计,秈︽﹍て巨
+class Person
+{
+public:
+	//1.篶硑ㄧ计
+	//⊿Τ,ぃノ糶void
+	//ㄧ计 籔摸
+	//篶硑ㄧ计Τ把计,祇ネ更
+	//承癸禜 篶硑ㄧ计穦笆秸ノ,τ秸ノΩ
+	Person()
+	{
+		cout << "Person 篶硑ㄧ计秸ノ" << endl;
+	}
+
+	//2.猂篶ㄧ计,秈︽睲瞶巨
+
+private:
+
+};
+
+
+
+//2.猂篶ㄧ计,秈︽睲瞶巨
+
+
+void test01()
+{
+	Person p;
+}
+
+int main()
+{
+
+	test01();
+
+
+
+	system("pause");
+	return 0;
+}
