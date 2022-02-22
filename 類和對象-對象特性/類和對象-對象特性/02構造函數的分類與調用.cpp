47e46f97fc3fec4b6d0c2dd2ef9a2222868af393
xvoz9522 <69076720+xvoz9522@users.noreply.github.com>
2022-02-22 20:01:12 +0800

    Add files via upload

---
+++ 類和對象-對象特性/類和對象-對象特性/02構造函數的分類與調用.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//1.篶硑ㄧ计だ摸籔秸ノ
+
+//だ摸
+class Person
+{
+public:
+	//篶硑ㄧ计
+	Person()
+	{
+		cout << "Person篶硑ㄧ计の秸ノ" << endl;
+	}
+	//猂篶ㄧ计
+	~Person()
+	{
+		cout << "Person猂篶ㄧ计の秸ノ" << endl;
+	}
+
+private:
+
+};
+
+Person::Person()
+{
+}
+
+Person::~Person()
+{
+}
+
+
+int main()
+{
+
+}
