cb517c1e7929f2d56db369b3e3b2cecd9b30fdbe
陳柏瑜 <69076720+xvoz9522@users.noreply.github.com>
2022-03-12 23:58:09 +0800

    Add files via upload

---
+++ 黑馬-友元/黑馬-友元/01-全局函數做友元.cpp
@@ -0,0 +1,52 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+////縱摸
+//class Building
+//{
+//	//GoodGayЫㄧ计琌,Building狟ね ,砐拜Buildingい╬ΤΘ
+//	friend void GoodGay(Building& Building);
+//public:
+//	Building()
+//	{
+//		m_SittingRoom = "芔";
+//		m_BedRoom = "";
+//	}
+//
+//
+//public:
+//	
+//	string m_SittingRoom;//芔
+//
+//private:
+//
+//	string m_BedRoom;//
+//
+//};
+//
+//
+////Ыㄧ计
+//void GoodGay(Building &Building)
+//{
+//	cout << "狟ねЫㄧ计 タ砐拜:: " << Building.m_SittingRoom << endl;
+//
+//	cout << "狟ねЫㄧ计 タ砐拜:: " << Building.m_BedRoom << endl;
+//}
+//
+//void test01()
+//{
+//
+//	Building building;
+//	GoodGay(building);
+//}
+//
+//
+//int main()
+//{
+//
+//	test01();
+//
+//	system("pause");
+//	return 0;
+//}
