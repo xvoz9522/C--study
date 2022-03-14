68e92f11410eb971213b257eab38ac115ccdefa0
陳柏瑜 <69076720+xvoz9522@users.noreply.github.com>
2022-03-14 21:14:41 +0800

    Add files via upload

---
+++ Eva的回家作業/Eva的回家作業/01填充題.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int t, a0, a1, a2, a3, a4;
+//	while (cin>>t)
+//	{
+//		while (t-->=1)
+//		{
+//			cin >> a0 >> a1 >> a2 >> a3;
+//			if ((a1 - a0) == (a2 - a1))
+//			{
+//				a4 = a3 * 2 - a2;
+//			}
+//			else
+//			{
+//				a4 = a3 * (a3 / a2);
+//			}
+//			cout << a0 << "" << a1 << "" << a2 << "" << a3 << "" << a4;
+//		}
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//}
